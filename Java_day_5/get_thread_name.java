29a18fa6996dbbbcdfa60bae92ef935f586d89d8
PRADEESHA <111351400+Pradeeshac@users.noreply.github.com>
2023-12-30 17:38:03 +0530

    Add files via upload

--- Java_day_5/get_thread_name.java
+++ Java_day_5/get_thread_name.java
@@ -4,38 +4,66 @@
  */
 package java_programs_PT;
 
+import java.io.File;
 import java.util.Scanner;
 
 /**
  *
- * @author pradee
+ * @author menaga
  */
-public class get_thread_name {
-    
-    public static void main(String[] args) {
+public class get_file {
+
+    private static final String FOLDER_PATH="C:\\Users\\menag\\Downloads\\MAR";
+
+    public static void main(String[] args) 
+    {
         Scanner scanner=new Scanner(System.in);
-
-        System.out.print("Enter name for Thread 1: ");
-        String t1name=scanner.nextLine();
-
-        System.out.print("Enter name for Thread 2: ");
-        String t2name=scanner.nextLine();
-        
-        MyRunnable obj=new MyRunnable();
-        Thread t1=new Thread(obj,t1name);
-        Thread t2=new Thread(obj,t2name);
-
-        t1.start();
-        t2.start();
-
+        System.out.print("Enter the file extension: ");
+        String extension=scanner.nextLine().toLowerCase();
+        System.out.println("Files with extension "+extension+" in folder "+FOLDER_PATH+":");
+        getFilesWithExtension(FOLDER_PATH,extension);
         scanner.close();
     }
-}
 
-class MyRunnable implements Runnable {
-    public void run() 
+    public static void getFilesWithExtension(String folderPath, String extension) 
     {
-        String threadName=Thread.currentThread().getName();
-        System.out.println("Thread name: "+threadName);
+        File folder=new File(folderPath);
+        File[] files=folder.listFiles();
+        boolean filesFound=false;
+        
+        if (files!=null)
+        {
+            for (File file:files) 
+            {
+                if (file.isFile()&&file.getName().toLowerCase().endsWith(extension)) 
+                {
+                    System.out.println(file.getName());
+                    filesFound=true;
+                }
+            }
+        }
+
+        if (!filesFound) 
+        {
+            System.out.println("No files found with extension "+ extension+ " in the Folder");
+        }
     }
 }
+
+
+/*
+ * 
+OUTPUT 1:
+Enter the file extension: .c
+Files with extension .c in folder C:\Users\menag\Downloads\MAR:
+No files found with extension .c in the Folder
+BUILD SUCCESSFUL (total time: 5 seconds)
+   
+OUTPUT 2 :
+Enter the file extension: .java
+Files with extension .java in folder C:\Users\menag\Downloads\MAR:
+get_thread_name.java
+BUILD SUCCESSFUL (total time: 9 seconds)
+
+
+ */
